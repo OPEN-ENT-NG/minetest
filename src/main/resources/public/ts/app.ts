7fe7d49d2a16a6780e2d5fdbf59c87cc08320aa9
jinhda douang <s>
2022-03-15 11:01:28 +0100

    wip

--- src/main/resources/public/ts/app.ts
+++ src/main/resources/public/ts/app.ts
@@ -1,15 +1,15 @@
 import { ng, routes } from 'entcore';
 import * as controllers from './controllers';
-// import * as directives from './directives';
+import * as directives from './directives';
 import * as services from './services';
 
 for(let controller in controllers){
     ng.controllers.push(controllers[controller]);
 }
 
-// for (let directive in directives) {
-// 	ng.directives.push(directives[directive]);
-// }
+for (let directive in directives) {
+	ng.directives.push(directives[directive]);
+}
 
 for (let service in services) {
 	ng.services.push(services[service]);
