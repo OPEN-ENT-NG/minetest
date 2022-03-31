5c889f860f942358fadd0d7ee151cf381e2b9187
CaroleHermouet <carole.hermouet@cgi.com>
2022-03-31 15:37:24 +0200

    feat(server): review

--- src/main/java/fr/openent/minetest/service/impl/DefaultMinetestService.java
+++ src/main/java/fr/openent/minetest/service/impl/DefaultMinetestService.java
@@ -1,6 +1,7 @@
 package fr.openent.minetest.service.impl;
 
 import fr.openent.minetest.config.MinetestConfig;
+import fr.openent.minetest.core.constants.Field;
 import fr.openent.minetest.service.MinetestService;
 import io.vertx.core.Future;
 import io.vertx.core.Promise;
@@ -33,8 +34,8 @@ public class DefaultMinetestService implements MinetestService {
                 .sendJsonObject(body , resp -> {
                     if(resp.failed() || resp.result().statusCode() != 200) {
                         String error = resp.failed() ? resp.cause().getMessage() :
-                                resp.result().bodyAsJsonObject().getJsonArray("message").getString(0) + ";" +
-                                        resp.result().bodyAsJsonObject().getString("data");
+                                resp.result().bodyAsJsonObject().getJsonArray(Field.MESSAGE).getString(0) + ";" +
+                                        resp.result().bodyAsJsonObject().getString(Field.DATA);
                         String message = String.format("[Minetest@%s::" + action + "]: An error has occurred " +
                                         "trough python server: %s",
                                 this.getClass().getSimpleName(), error);
