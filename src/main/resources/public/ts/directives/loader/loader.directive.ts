4f8a3dc9409eddf33ad356cd4df6b823bab0ba8f
Samuel JOLLOIS <sjollois@ensc.fr>
2022-09-15 09:34:01 +0200

    feat(loader): #MEX-199 add loader when getting minetest worlds

--- src/main/resources/public/ts/directives/loader/loader.directive.ts
+++ src/main/resources/public/ts/directives/loader/loader.directive.ts
@@ -1,19 +1,43 @@
-import {ng} from 'entcore';
+import {ng} from "entcore";
+import {RootsConst} from "../../core/constants/roots.const";
+import {IScope} from "angular";
 
-export const LoaderDirective = ng.directive('loader', () => {
+interface IViewModel {
+
+    // props
+    minHeight: string;
+
+}
+
+class Controller implements ng.IController, IViewModel {
+    minHeight: string;
+
+    constructor(private $scope: IScope) {
+    }
+
+    $onInit() {
+    }
+
+    $onDestroy() {
+    }
+}
+
+function directive() {
     return {
         restrict: 'E',
+        templateUrl: `${RootsConst.directive}loader/loader.html`,
         scope: {
-            title: '=',
-            minHeight: '='
+            minHeight: '=',
         },
-        template: `
-        <div class="loader-container" ng-style="{'min-height': minHeight}">
-            <div>
-                <div class="loader"></div>
-                <p>[[title]]</p>
-            </div>
-        </div>
-        `,
-    };
-});
+        controllerAs: 'vm',
+        bindToController: true,
+        controller: ['$scope', Controller],
+        link: function (scope: ng.IScope,
+                        element: ng.IAugmentedJQuery,
+                        attrs: ng.IAttributes,
+                        vm: ng.IController) {
+
+        }
+    }
+}
+export const loader = ng.directive('loader', directive)
