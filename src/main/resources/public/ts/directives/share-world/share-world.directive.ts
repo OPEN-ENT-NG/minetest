6b6a5ab6ded832ed1878a98cf4363db2466034a3
Samuel Jollois <sjollois@ensc.fr>
2022-09-23 16:51:45 +0200

    feat(share): #MEX-61 show owner in share panel (#48)

--- src/main/resources/public/ts/directives/share-world/share-world.directive.ts
+++ src/main/resources/public/ts/directives/share-world/share-world.directive.ts
@@ -1,4 +1,4 @@
-import {ng} from "entcore";
+import {model, ng} from "entcore";
 import {RootsConst} from "../../core/constants/roots.const";
 import {IScope} from "angular";
 import {IImportWorld, IWorld} from "../../models";
@@ -6,6 +6,8 @@ import {IImportWorld, IWorld} from "../../models";
 interface IViewModel {
     openShareLightbox(): void;
     closeShareLightbox(): void;
+    canEditShareItem(args: any) : boolean;
+    onShareFeed(data: any, resource: IWorld | IImportWorld, actions: any[]) : void;
 
     lightbox: any;
 
@@ -35,6 +37,48 @@ class Controller implements ng.IController, IViewModel {
 
     closeShareLightbox(): void {
         this.lightbox.share = false;
+    }
+
+    //code duplicate from workspace
+    canEditShareItem(args: any) : boolean {
+        if (args.type == "user") {
+            const uniqOwnerIds : string[] = [this.world].map((sha: IWorld | IImportWorld) => sha.owner.userId)
+                .filter((elem : string, pos: number, arr: string[]) => {
+                return arr.indexOf(elem) == pos;
+            });
+            //can edit only if the user is not the owner
+            if (uniqOwnerIds.length == 1) {
+                return uniqOwnerIds[0] != args.id;
+            }
+        }
+        return true;
+    }
+
+    //code duplicate from workspace
+    onShareFeed(data: any, resource: IWorld | IImportWorld, actions: any[]) : void {
+        const userId : string = resource.owner.userId;
+        //if owner is current user => skip
+        if (userId == model.me.userId) {
+            return;
+        }
+        //if owner is in shared or inherithshared=>skip
+        if (data.users["checked"][userId] || (data.users["checkedInherited"] && data.users["checkedInherited"][userId])) {
+            return;
+        }
+        //get ownername
+        const userName : string = resource.owner.displayName;
+        //add owner as manager => collect all actions
+        const actionsNames : string[] = actions.map(a => a.name).reduce((prev, current) => prev.concat(current), [])
+        if (!actionsNames.length) {
+            throw "could not found actions"
+        }
+        //add owner as managaer=>push user visible and checked
+        data.users["checked"][userId] = actionsNames;
+        data.users["visibles"].push({
+            id: userId,
+            username: userName,
+            type: "user"
+        })
     }
 }
 
